1143cce7bdae7adcfc26aa66d200845aa6f75002
Paticia Morad <pjmorad@gmail.com>
2018-01-23 21:46:53 -0600

    added function to customize google map colors

--- public/index.js
+++ public/index.js
@@ -141,7 +141,10 @@ var EventInfoPage = {
       event: { venue: {} }
     };
   },
-  created: function() {
+  created: function() {},
+  mounted: function() {
+    console.log("event info mounted");
+
     console.log("EventInfoPage created", this.$route.query);
     axios
       .get(
@@ -154,48 +157,174 @@ var EventInfoPage = {
         function(response) {
           this.event = response.data;
           console.log(this.event);
+          this.setupMap();
         }.bind(this)
       );
   },
-  mounted: function() {
-    axios
-      .get(
-        "/v1/events/" +
-          this.$route.params.id +
-          "/?urlname=" +
-          this.$route.query.urlname
-      )
-      .then(
-        function(response) {
-          this.event = response.data;
-          console.log(this.event);
-          var map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: 41.8781, lng: -87.6298 },
-            zoom: 12
-          });
-          var location = {
-            lat: this.event.venue.lat,
-            lng: this.event.venue.lon
-          };
-          console.log(location);
-
-          var contentString = this.event.venue.address_1;
-          var infowindow = new google.maps.InfoWindow({
-            content: contentString
-          });
-
-          var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            title: "Location"
-          });
-          marker.addListener("click", function() {
-            infowindow.open(map, marker);
-          });
-        }.bind(this)
+  methods: {
+    setupMap: function() {
+      // and the name to be displayed on the map type control.
+      var styledMapType = new google.maps.StyledMapType(
+        [
+          { elementType: "geometry", stylers: [{ color: "#ebe3cd" }] },
+          { elementType: "labels.text.fill", stylers: [{ color: "#523735" }] },
+          {
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#f5f1e6" }]
+          },
+          {
+            featureType: "administrative",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#c9b2a6" }]
+          },
+          {
+            featureType: "administrative.land_parcel",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#dcd2be" }]
+          },
+          {
+            featureType: "administrative.land_parcel",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#ae9e90" }]
+          },
+          {
+            featureType: "landscape.natural",
+            elementType: "geometry",
+            stylers: [{ color: "#dfd2ae" }]
+          },
+          {
+            featureType: "poi",
+            elementType: "geometry",
+            stylers: [{ color: "#dfd2ae" }]
+          },
+          {
+            featureType: "poi",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#93817c" }]
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry.fill",
+            stylers: [{ color: "#a5b076" }]
+          },
+          {
+            featureType: "poi.park",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#447530" }]
+          },
+          {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [{ color: "#f5f1e6" }]
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [{ color: "#fdfcf8" }]
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry",
+            stylers: [{ color: "#f8c967" }]
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#e9bc62" }]
+          },
+          {
+            featureType: "road.highway.controlled_access",
+            elementType: "geometry",
+            stylers: [{ color: "#e98d58" }]
+          },
+          {
+            featureType: "road.highway.controlled_access",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#db8555" }]
+          },
+          {
+            featureType: "road.local",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#806b63" }]
+          },
+          {
+            featureType: "transit.line",
+            elementType: "geometry",
+            stylers: [{ color: "#dfd2ae" }]
+          },
+          {
+            featureType: "transit.line",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#8f7d77" }]
+          },
+          {
+            featureType: "transit.line",
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#ebe3cd" }]
+          },
+          {
+            featureType: "transit.station",
+            elementType: "geometry",
+            stylers: [{ color: "#dfd2ae" }]
+          },
+          {
+            featureType: "water",
+            elementType: "geometry.fill",
+            stylers: [{ color: "#b9d3c2" }]
+          },
+          {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#92998d" }]
+          }
+        ],
+        { name: "Styled Map" }
       );
+
+      console.log(
+        "this event is....",
+        this.event,
+        this.event.venue,
+        this.event.venue.address_1
+      );
+      var map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 41.8781, lng: -87.6298 },
+        zoom: 12,
+        mapTypeControlOptions: {
+          mapTypeIds: [
+            "roadmap",
+            "satellite",
+            "hybrid",
+            "terrain",
+            "styled_map"
+          ]
+        }
+      });
+      map.mapTypes.set("styled_map", styledMapType);
+      map.setMapTypeId("styled_map");
+      console.log("Here's the map:", map);
+
+      var location = {
+        lat: this.event.venue.lat,
+        lng: this.event.venue.lon
+      };
+      console.log(location);
+
+      var contentString = this.event.venue.address_1;
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        title: "Location"
+      });
+      marker.addListener("click", function() {
+        infowindow.open(map, marker);
+      });
+    }
   },
-  methods: {},
   computed: {},
   filters: {
     shortDescription: function(text) {
